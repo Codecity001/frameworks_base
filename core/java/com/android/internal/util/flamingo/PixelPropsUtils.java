57d2344011752f4768612b388e94ad73347d67d1
Harish <sanganiharish263@gmail.com>
2022-12-09 18:16:03 +0530

    Revert "PixelPropsUtils: Spoof Nexus 6P for GMS unstable process"
    This reverts commit 0ef41522f801afb8757972d69d436744c1b67b90.

--- core/java/com/android/internal/util/flamingo/PixelPropsUtils.java
+++ core/java/com/android/internal/util/flamingo/PixelPropsUtils.java
@@ -38,7 +38,7 @@ public final class PixelPropsUtils {
 
     private static volatile boolean sIsGms = false;
     public static final String PACKAGE_GMS = "com.google.android.gms";
-    public static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
+    public static final String PROCESS_GMS_UNSTABLE = "com.google.android.gms.unstable";
 
     private static final Map<String, Object> commonProps = Map.ofEntries(
         entry("BRAND", "google"),
@@ -64,13 +64,6 @@ public final class PixelPropsUtils {
         entry("FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys")
     );
 
-    private static final Map<String, String> anglerProps = Map.ofEntries(
-        entry("DEVICE", "angler"),
-        entry("PRODUCT", "angler"),
-        entry("MODEL", "angler"),
-        entry("FINGERPRINT", "google/angler/angler:6.0/MDB08L/2343525:user/release-keys")
-    );
-
     private static final Set<String> packagesToChange = Set.of(
         "com.google.android.apps.customization.pixel",
         "com.google.android.apps.fitness",
@@ -88,6 +81,7 @@ public final class PixelPropsUtils {
         "com.google.android.configupdater",
         "com.google.android.dialer",
         "com.google.android.ext.services",
+        "com.google.android.gms",
         "com.google.android.gms.location.history",
         "com.google.android.googlequicksearchbox",
         "com.google.android.gsf",
@@ -120,7 +114,14 @@ public final class PixelPropsUtils {
         }
         if (packagesToChange.contains(packageName)) {
             commonProps.forEach(PixelPropsUtils::setPropValue);
-            cheetahProps.forEach(PixelPropsUtils::setPropValue);
+            cheetahProps.forEach((key, value) -> {
+                if (key.equals("MODEL") && packageName.equals(PACKAGE_GMS) 
+                    && PROCESS_GMS_UNSTABLE.equals(Application.getProcessName()))) {
+                    sIsGms = true;
+                } else {
+                    setPropValue(key, value);
+                }
+            });
         } else if (packagesToChangePixelXL.contains(packageName)) {
             commonProps.forEach(PixelPropsUtils::setPropValue);
             marlinProps.forEach(PixelPropsUtils::setPropValue);
@@ -128,11 +129,6 @@ public final class PixelPropsUtils {
         // Set proper indexing fingerprint
         if (packageName.equals("com.google.android.settings.intelligence")) {
             setPropValue("FINGERPRINT", Build.DATE);
-        }
-        // device integrity check hacks
-        if (packageName.equals(PACKAGE_GMS) && PROCESS_GMS_UNSTABLE.equals(Application.getProcessName())) {
-            setPropValue("FINGERPRINT", anglerProps.get("FINGERPRINT"));
-            setPropValue("MODEL", anglerProps.get("MODEL"));
         }
     }
 
